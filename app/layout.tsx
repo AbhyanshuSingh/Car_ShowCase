7d64a22b5b77a2a557d0056ed30176829db0f042
AbhyanshuSingh <124425348+AbhyanshuSingh@users.noreply.github.com>
2025-06-11 14:26:01 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+
+export const metadata = {
+  title: 'Car Hub',
+  description: "Discover world's best car showcase application.",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
